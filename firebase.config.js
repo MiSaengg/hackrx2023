3abdd07957c412ac6e6d996618f09c120eabcb3c
David Cho <gcho13@my.bcit.ca>
2023-07-15 15:08:19 -0400

    Firebase config

---
+++ firebase.config.js
@@ -0,0 +1,21 @@
+// firebase.config.js
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+};
+
+console.log(firebaseConfig); 
+
+const app = initializeApp(firebaseConfig);
+
+console.log("Firebase initialized");
+
+export default app; 
