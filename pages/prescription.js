acb49bb5bf398e5670a30aa231d914f730172022
David Cho <gcho13@my.bcit.ca>
2023-07-16 13:00:53 -0400

    Styles

--- pages/prescription.js
+++ pages/prescription.js
@@ -13,6 +13,8 @@ import { db, auth } from "../firebase/firebase.config";
 import ReactModal from "react-modal";
 import { arrayUnion } from "firebase/firestore";
 import { setDoc } from "firebase/firestore";
+import "../public/styles.css";
+import Link from "next/link";
 
 const PrescriptionPage = () => {
   const router = useRouter();
@@ -63,14 +65,19 @@ const PrescriptionPage = () => {
       Pharmacist: userName,
       "License Number": licenseNumber,
       Address: address,
-      "E-Signature": eSignature,
+      "e-Signature": eSignature,
       Aliment: selectedAliment,
       Drug: selectedDrug,
       Dosage: dosage,
       "Times Per Day": timesPerDay,
       "Additional Info": additionalInfo,
-      Date: new Date().toISOString(), 
+      Date: new Date().toLocaleDateString(),
     };
+
+    if (!eSignature) {
+      alert("Please provide your e-Signature");
+      return;
+    }
 
     const response = await fetch("/api/send-email", {
       method: "POST",
@@ -81,9 +88,7 @@ const PrescriptionPage = () => {
     });
 
     if (response.ok) {
-      console.log("Email sent successfully");
 
-      // Add the prescription to the user's records collection
       const userRecordsRef = doc(collection(db, "records"), currentUserId);
 
       await setDoc(
@@ -93,6 +98,8 @@ const PrescriptionPage = () => {
         },
         { merge: true }
       );
+      window.alert("Email sent successfully");
+      setIsModalOpen(false);
     } else {
       const errorData = await response.json();
       console.error(errorData.message);
@@ -184,91 +191,145 @@ const PrescriptionPage = () => {
   };
 
   return (
-    <div>
-      <h1>Prescription Page</h1>
+    <main className="flex flex-col items-center justify-center">
+      <Link href="/main">
+        <div className="flex items-center justify-center p-6 md:p-24">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            className="h-6 w-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M10 19l-7-7m0 0l7-7m-7 7h18"
+            ></path>
+          </svg>
+          Back
+        </div>
+      </Link>
+      <div className="flex items-center justify-center">
+        <div className="w-full max-w-lg px-8 py-8 bg-white shadow-md rounded-lg flex flex-col justify-center font-mono text-lg">
+          <h1 className="text-2xl font-bold mb-4">Prescription Page</h1>
 
-      <input
-        list="email-suggestions"
-        type="email"
-        value={email}
-        onChange={handleEmailChange}
-        placeholder="Email"
-      />
+          <input
+            list="email-suggestions"
+            type="email"
+            value={email}
+            onChange={handleEmailChange}
+            placeholder="Email"
+            className="outlined-input"
+          />
 
-      <datalist id="email-suggestions">
-        {searchResults.map((user, index) => (
-          <option key={index} value={user.email}>
-            {user.email}
-          </option>
-        ))}
-      </datalist>
+          <datalist id="email-suggestions">
+            {searchResults.map((user, index) => (
+              <option key={index} value={user.email}>
+                {user.email}
+              </option>
+            ))}
+          </datalist>
 
-      <select onChange={handleAlimentChange}>
-        {aliments.map((aliment, index) => (
-          <option key={index} value={aliment.id}>
-            {aliment.id}
-          </option>
-        ))}
-      </select>
+          <select onChange={handleAlimentChange} className="outlined-input">
+            {aliments.map((aliment, index) => (
+              <option key={index} value={aliment.id}>
+                {aliment.id}
+              </option>
+            ))}
+          </select>
 
-      <select value={selectedDrug} onChange={handleDrugChange}>
-        {specifiedDrugs.map((drug, index) => (
-          <option key={index} value={drug}>
-            {drug}
-          </option>
-        ))}
-      </select>
+          <select
+            value={selectedDrug}
+            onChange={handleDrugChange}
+            className="outlined-input"
+          >
+            {specifiedDrugs.map((drug, index) => (
+              <option key={index} value={drug}>
+                {drug}
+              </option>
+            ))}
+          </select>
 
-      <input
-        type="text"
-        value={dosage}
-        onChange={(e) => setDosage(e.target.value)}
-        placeholder="Dosage"
-      />
+          <input
+            type="text"
+            value={dosage}
+            onChange={(e) => setDosage(e.target.value)}
+            placeholder="Dosage"
+            className="outlined-input"
+          />
 
-      <input
-        type="number"
-        value={timesPerDay}
-        onChange={(e) => {
-          if (e.target.value < 1) {
-            setTimesPerDay(1);
-          } else {
-            setTimesPerDay(e.target.value);
-          }
-        }}
-        placeholder="Number of times per day"
-        min="1"
-      />
+          <input
+            type="number"
+            value={timesPerDay}
+            onChange={(e) => {
+              if (e.target.value < 1) {
+                setTimesPerDay(1);
+              } else {
+                setTimesPerDay(e.target.value);
+              }
+            }}
+            placeholder="Number of times per day"
+            min="1"
+            className="outlined-input"
+          />
 
-      <textarea
-        value={additionalInfo}
-        onChange={(e) => setAdditionalInfo(e.target.value)}
-        placeholder="Additional Information"
-      />
+          <textarea
+            value={additionalInfo}
+            onChange={(e) => setAdditionalInfo(e.target.value)}
+            placeholder="Additional Information"
+            className="outlined-input"
+          />
 
-      <button onClick={handleSend}>Send</button>
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4"
+            onClick={handleSend}
+          >
+            Send
+          </button>
 
-      <ReactModal
-        isOpen={isModalOpen}
-        onRequestClose={() => setIsModalOpen(false)}
-        ariaHideApp={false}
-      >
-        <button onClick={() => setIsModalOpen(false)}>X</button>
-        <h2>Confirmation</h2>
-        <p>Pharmacist: {userName}</p>
-        <p>License Number: {licenseNumber}</p>
-        <p>Address: {address}</p>
+          <ReactModal
+            isOpen={isModalOpen}
+            onRequestClose={() => setIsModalOpen(false)}
+            ariaHideApp={false}
+          >
+            <div className="min-h-screen flex items-center justify-center">
+              <div className="w-full max-w-lg px-8 py-8 bg-white shadow-md rounded-lg flex flex-col justify-center font-mono text-lg">
+                <button
+                  onClick={() => setIsModalOpen(false)}
+                  className="text-right font-bold"
+                >
+                  X
+                </button>
+                <h2>Confirmation</h2>
+                <p>Pharmacist: {userName}</p>
+                <p>License Number: {licenseNumber}</p>
+                <p>Address: {address}</p>
 
-        <input
-          type="text"
-          value={eSignature}
-          onChange={(e) => setESignature(e.target.value)}
-          placeholder="E-Signature"
-        />
+                <input
+                  type="text"
+                  value={eSignature}
+                  onChange={(e) => setESignature(e.target.value)}
+                  placeholder="e-Signature"
+                  className="outlined-input"
+                  required
+                />
 
-        <button onClick={handleEmailSend}>Send Email</button>
-      </ReactModal>
-    </div>
+                <button
+                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4"
+                  onClick={handleEmailSend}
+                >
+                  Send Email
+                </button>
+              </div>
+              <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]"></div>
+            </div>
+          </ReactModal>
+        </div>
+        <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]"></div>
+      </div>
+    </main>
   );
 };
 
